3e2c9eb4dd231a6d1e42738a2627238dfbf4da84
Leonardo Cardoso Espindola <zzleleoz@gmail.com>
2026-05-07 21:55:06 -0300

    CSS aplicado e lógica de prioridade

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,24 +8,27 @@ addButton.addEventListener("click", () => {
   const dateInput = document.querySelector(".js-date-input");
   const priorityInput = document.querySelector(".js-priority-input");
 
-  tasks.push({
-    id: id++,
-    task: typingInput.value,
-    date: dateInput.value,
-    priority: priorityInput.value,
-  });
-
-  renderTasks();
-
-  typingInput.value = "";
-  dateInput.value = "";
-  priorityInput.value = "1";
-
-  console.log(tasks);
+  if (dateInput.value != '' && typingInput != '') {
+    tasks.push({
+      id: id++,
+      task: typingInput.value,
+      date: dateInput.value,
+      priority: priorityInput.value,
+    });
+  
+    renderTasks();
+  
+    typingInput.value = "";
+    dateInput.value = "";
+    priorityInput.value = "1";
+  
+  } else {
+    alert('Está faltando alguma informação da sua tarefa! (nome ou data)')
+  }
 });
 
 
-const tasksContainerDelete = document.querySelector(".js-tasks-div");
+const tasksContainerDelete = document.querySelector(".js-all-tasks");
 tasksContainerDelete.addEventListener('click', (event) => {
   if (event.target.classList.contains("js-delete-button")) {
     const taskId = Number(event.target.parentElement.dataset.id);
@@ -41,16 +44,55 @@ tasksContainerDelete.addEventListener('click', (event) => {
 
 
 function renderTasks() {
-  const tasksContainer = document.querySelector(".js-tasks-div");
-  tasksContainer.innerHTML = "";
+  const tasksContainerHigh = document.querySelector(".js-tasks-div-high");
+  const tasksContainerMedium = document.querySelector(".js-tasks-div-medium");
+  const tasksContainerLow = document.querySelector(".js-tasks-div-low");
+  tasksContainerHigh.innerHTML = "";
+  tasksContainerMedium.innerHTML = "";
+  tasksContainerLow.innerHTML = "";
 
-  tasks.forEach((task) => {
-    tasksContainer.innerHTML += `
-        <div data-id="${task.id}">
-          <p style="display: inline">${task.task}</p>
-          <p style="display: inline">${task.date}</p>
-          <button class="js-delete-button" >DELETE</button>
-        </div>
-  `;
+    tasks.forEach((task) => {
+
+    if (task.priority == 1) {
+      tasksContainerLow.innerHTML += `
+          <div data-id="${task.id}" id="${task.id}">
+            <div class="tasks-date-div">
+            <div class="text-div">
+              <p style="display: inline" class="task-text">${task.task}</p>
+            </div>
+              <p style="display: inline" class="date-text">${task.date}</p>
+            </div>
+            <button class="js-delete-button delete-button">DELETE</button>
+          </div>
+    `;
+      document.getElementById(`${task.id}`).classList.add("task-cont-low");
+    } else if (task.priority == 2) {
+      tasksContainerMedium.innerHTML += `
+          <div data-id="${task.id}" id="${task.id}">
+            <div class="tasks-date-div">
+            <div class="text-div">
+              <p style="display: inline" class="task-text">${task.task}</p>
+            </div>
+              <p style="display: inline" class="date-text">${task.date}</p>
+            </div>
+            <button class="js-delete-button delete-button">DELETE</button>
+          </div>
+    `;
+      document.getElementById(`${task.id}`).classList.add("task-cont-medium");
+    } else if (task.priority == 3) {
+      tasksContainerHigh.innerHTML += `
+          <div data-id="${task.id}" id="${task.id}">
+            <div class="tasks-date-div">
+            <div class="text-div">
+              <p style="display: inline" class="task-text">${task.task}</p>
+            </div>
+              <p style="display: inline" class="date-text">${task.date}</p>
+            </div>
+            <button class="js-delete-button delete-button">DELETE</button>
+          </div>
+    `;
+      document.getElementById(`${task.id}`).classList.add("task-cont-high");
+    }
+
   });
 }
